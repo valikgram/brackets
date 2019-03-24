3347263d646f329605e95ad911e0cc9d3c361764
valikgram <work.v@mail.ru>
2019-03-24 20:18:08 +0300

    add cycle

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var brackets = [];
+  for (var i = 0; i < str.length; i++) {
+    for (var j = 0; j < bracketsConfig.length; j++) {
+      if(bracketsConfig[j][0] === bracketsConfig[j][1] && bracketsConfig[j][1] === str[i]) {
+        if(str[i] === brackets[brackets.length - 1]) {
+          brackets.pop();
+        } else 
+        brackets.push(str[i]);
+        } else {
+        if (str[i] === bracketsConfig[j][0])
+        brackets.push(str[i]);
+        if ((str[i] === bracketsConfig[j][1]) && (brackets[brackets.length - 1] === bracketsConfig[j][0]))
+        brackets.pop();
+      }
+    }
+  }
+return brackets.length === 0;
 }
